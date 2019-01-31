d2801c8158447afc4f5fdd16eafbd7b05c548110
isaacjv <shaknbake58@gmail.com>
2019-01-31 13:49:07 -0500

    tweaks

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -27,11 +27,6 @@ const account ={
     }
 }
 
-//add income array into the account
-//set up addIncome method -> description, amount
-//revise the get accout summary to subtract expesnse from income and provide account balance
-//return: Name has a balance of $10. $100 in income. $90 in expenses
-
 account.addExpense('Rent', 950);
 account.addExpense('Coffee', 2);
 account.addIncome('Job', 1000);
