03c70600c6d4079fe5974e755d1a3400530b79a7
isaacjv <shaknbake58@gmail.com>
2019-01-31 20:46:01 -0500

    learning to do a bit with the DOM

--- notes-app/note-app.js
+++ notes-app/note-app.js
@@ -1,3 +1,14 @@
+const notes = [{
+    Title: 'My next trip',
+    Body: 'I would like to go somewhere warm and beachy'
+},{
+    Title: 'Habits to work on',
+    Body: 'Exercise, and Eating a bit better'
+},{
+    Title: 'Office Modifications',
+    Body: 'get some new 4k monitors'
+}];
+
 // DOM - Document Object Model
 
 // Query and remove
@@ -11,3 +22,8 @@ ps.forEach(function (p) {
     //console.log(p.textContent);
     //p.remove();
 })
+
+// add a new element
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'Programmatic creation of elements is fun';
+document.body.appendChild(newParagraph);
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,36 @@
+const todos = [{
+    name: 'make bed',
+    isComplete: false
+ }, {
+    name: 'brush teeth',
+    isComplete: true
+}, {
+    name: 'get milk',
+    isComplete: true
+}, {
+    name: 'walk the dog',
+    isComplete: false
+ }, {
+    name: 'go to the gym',
+    isComplete: false
+ }];
 
-const pscan = document.querySelectorAll('p');
-pscan.forEach(function (p) {
-    if(p.textContent.toLowerCase().includes('the')) {
-        p.remove();
+ //print a summary message for how many todos are not complete <p tag>
+ // add a p for each to do above. use the text value of object as the visible text.
+
+let todoCount = 0;
+todos.forEach(function (todo) {
+    if(!todo.isComplete) {
+        todoCount += 1;
     }
+})
+
+const summary = document.createElement('p');
+summary.textContent = `You have ${todoCount} todos left.`;
+document.body.appendChild(summary);
+
+todos.forEach( function(todo){
+    let newP = document.createElement('p');
+    newP.textContent = todo.name;
+    document.body.appendChild(newP);
 })
