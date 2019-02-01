dc2b344654d4e1511ef1bcba48f385ed099a65c2
isaacjv <shaknbake58@gmail.com>
2019-01-31 20:55:46 -0500

    couple of tweaks

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -18,18 +18,15 @@ const todos = [{
  //print a summary message for how many todos are not complete <p tag>
  // add a p for each to do above. use the text value of object as the visible text.
 
-let todoCount = 0;
-todos.forEach(function (todo) {
-    if(!todo.isComplete) {
-        todoCount += 1;
-    }
+const todoCount = todos.filter(function (todo) {
+    return !todo.isComplete;
 })
 
-const summary = document.createElement('p');
-summary.textContent = `You have ${todoCount} todos left.`;
+const summary = document.createElement('h2');
+summary.textContent = `You have ${todoCount.length} todos left`;
 document.body.appendChild(summary);
 
-todos.forEach( function(todo){
+todos.forEach(function(todo){
     let newP = document.createElement('p');
     newP.textContent = todo.name;
     document.body.appendChild(newP);
